e7e6f1a6135e79a61b54c30dd8600d8b1348fc4b
LogicNovaTeam <logicnova.team@gmail.com>
2026-07-09 12:52:50 -0700

    Initial LogicNova Website

--- js/script.js
+++ js/script.js
@@ -172,3 +172,24 @@ navLinks.forEach(link => {
     });
 });
 
+/* ===========================
+   Mobile Menu
+=========================== */
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
+
+if (menuToggle && navbar) {
+
+    menuToggle.addEventListener("click", () => {
+        navbar.classList.toggle("active");
+    });
+
+    document.querySelectorAll(".navbar a").forEach(link => {
+        link.addEventListener("click", () => {
+            navbar.classList.remove("active");
+        });
+    });
+
+}
+
